c8e6e1a33713c7a93e88272582ad007f87977d5e
da-matsumoto <da.mats1990@gmail.com>
2018-06-01 13:40:05 +0900

    web.phpを修正

--- middleware/routes/web.php
+++ middleware/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Middleware\HelloMiddleware;
+
+Route::get('hello', 'HelloController@index')->middleware(HelloMiddleware::class);
